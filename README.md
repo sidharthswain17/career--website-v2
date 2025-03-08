b15ee002ab4c3033ed78fbf1c753dba615749736
Sidharth Swain <swainsidharth17@gmail.com>
2025-03-08 14:00:38 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# career--website
+a career website
---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,jsonify
+
+app=Flask(__name__)
+jobs = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bangalore',
+        'salary': 'Rs 12,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Delhi',
+        'salary': 'Rs 22,00,000'
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'Bangalore',
+        'salary': 'Rs 2,00,000'
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'Bangalore',
+        'salary': 'Rs 15,00,000'
+    }
+]
+
+    
+    
+@app.route("/")
+def hello_world():
+    return render_template('home.html',JOBS=jobs)
+
+@app.route("/jobs")
+def jobs_info():
+    return jsonify(jobs)
+
+if __name__=="__main__":
+    app.run(debug=True)
