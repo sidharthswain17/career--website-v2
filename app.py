f95b599fba31d123f9b017823ea72129b7afcf4f
siddy <swainsidharth17@gmail.com>
2025-03-10 20:58:27 +0530

    registration data into db

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template,jsonify
-from database import laod_jobs_from_db
+from flask import Flask,render_template,jsonify,url_for,request
+from database import laod_jobs_from_db,load_job_from_db,insert_to_db
 
 
 app=Flask(__name__)
@@ -8,6 +8,23 @@ app=Flask(__name__)
 def hello_world():
     jobs=laod_jobs_from_db()
     return render_template('home.html',jobs=jobs)
+
+@app.route("/job/<int:id>")
+def show_job(id):
+    job =load_job_from_db(id)
+    if not job:
+        return "Job not found",404
+    return render_template('job_page.html',job=job)
+
+@app.route("/job/<int:id>/apply",methods=["POST"])
+def apply_to_job(id):
+    data=request.form
+    job=load_job_from_db(id)
+    # return jsonify(data)
+    insert_to_db(data, id)
+    return render_template("application.html",application=data,job=job)
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
--- database.py
+++ database.py
@@ -9,7 +9,27 @@ def laod_jobs_from_db():
       jobs = list(result)  # Already a list of dictionaries
         # Print the results
     return jobs  
-    
-    
-    
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+   result =conn.execute(text("SELECT * FROM jobs WHERE id= :val"),{"val":id}).mappings().first()
+   if result is None:
+     return None
+   else:
+    return dict(result)
+   
+def insert_to_db(data,job_id):
+  with engine.connect() as conn:
+    with conn.begin():
+      query = text(" INSERT INTO applications (job_id, full_name, email, linkedin, education, experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin, :education, :experience, :resume_url)")
+
+      conn.execute(query,{"job_id":job_id,
+                          "full_name":data["full_name"],
+                          "email":data["email"],
+                          "linkedin":data["linkedin"],
+                          "education":data["education"],
+                          "experience":data["experience"],
+                          "resume_url":data["resume_url"]})
+      conn.commit()
+   
     
