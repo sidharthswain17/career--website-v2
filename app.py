4f977189c1f63e360dfd3f133a000ef88191b46f
siddy <swainsidharth17@gmail.com>
2025-03-10 14:22:30 +0530

    database connectivity

--- app.py
+++ app.py
@@ -3,46 +3,13 @@ from database import laod_jobs_from_db
 
 
 app=Flask(__name__)
-# jobs = [
-#     {
-#         'id': 1,
-#         'title': 'Data Analyst',
-#         'location': 'Bangalore',
-#         'salary': 'Rs 12,00,000'
-#     },
-#     {
-#         'id': 2,
-#         'title': 'Data Scientist',
-#         'location': 'Delhi',
-#         'salary': 'Rs 22,00,000'
-#     },
-#     {
-#         'id': 3,
-#         'title': 'Frontend Engineer',
-#         'location': 'Bangalore',
-#         'salary': 'Rs 2,00,000'
-#     },
-#     {
-#         'id': 4,
-#         'title': 'Backend Engineer',
-#         'location': 'Bangalore',
-#         'salary': 'Rs 15,00,000'
-#     }
-# ]
-
-    
- 
 
 @app.route("/")
 def hello_world():
     jobs=laod_jobs_from_db()
     return render_template('home.html',jobs=jobs)
 
-@app.route("/jobs")
-def jobs_info():
-    return jsonify(jobs)
-
 if __name__=="__main__":
     app.run(debug=True)
 
-    # wddw
+  
